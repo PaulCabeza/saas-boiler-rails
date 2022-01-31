909d6c02e3ba083574f4a361ac196fbd576d12a5
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-01-30 19:51:56 -0600

    commented on application.rb config load_defaults

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Phil2
     #     Devise::PasswordsController.layout "sign"
     #   end   # Devise
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
